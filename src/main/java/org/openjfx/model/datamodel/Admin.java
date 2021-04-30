a809baeb569739abd48a9de3b5d7969d640d8225
Kai Sun <ksun0017@student.monash.edu>
2021-04-30 17:29:41 +1000

    Update Admin.java

--- src/main/java/org/openjfx/model/datamodel/Admin.java
+++ src/main/java/org/openjfx/model/datamodel/Admin.java
@@ -3,6 +3,10 @@ package org.openjfx.model.datamodel;
 import org.openjfx.model.datamodel.abstracts.User;
 
 public class Admin extends User {
+    public Admin(String userName, String password) {
+        super(userName, password);
+    }
+
     @Override
     public boolean isAdmin() {
         return true;
