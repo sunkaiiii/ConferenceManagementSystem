8b8b8021997b424be215de861db2fcaa92490a4b
Kai Sun <ksun0017@student.monash.edu>
2021-05-15 16:27:31 +1000

    animation on the alert dialog

--- src/main/java/org/openjfx/controllers/dialog/FinalDecisionAlertView.java
+++ src/main/java/org/openjfx/controllers/dialog/FinalDecisionAlertView.java
@@ -1,14 +1,21 @@
 package org.openjfx.controllers.dialog;
 
+import javafx.animation.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.CacheHint;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 import org.openjfx.controllers.PageNames;
 import org.openjfx.controllers.dialog.absdialog.AbstractAlertDialog;
 import org.openjfx.controllers.dialog.absdialog.ButtonStyle;
@@ -17,7 +24,6 @@ import org.openjfx.helper.SceneHelper;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class FinalDecisionAlertView extends AbstractAlertDialog {
 
@@ -29,6 +35,9 @@ public class FinalDecisionAlertView extends AbstractAlertDialog {
 
     @FXML
     private Label alertContent;
+
+    @FXML
+    private Parent contentBody;
 
     private Parent root;
 
@@ -93,8 +102,39 @@ public class FinalDecisionAlertView extends AbstractAlertDialog {
 
     @Override
     public void show(Node caller) {
-        caller.setEffect(new GaussianBlur());
+        GaussianBlur blur = new GaussianBlur(0);
+        caller.setEffect(blur);
+        caller.setCache(true);
+        caller.setCacheHint(CacheHint.SPEED);
+        IntegerProperty value = new SimpleIntegerProperty(0);
+        value.addListener((observable, oldV, newV) -> {
+            if (newV.intValue() == 24) {
+                caller.setCacheHint(CacheHint.QUALITY);
+            }
+            blur.setRadius(newV.intValue());
+        });
+        Timeline timeline = new Timeline();
+        final KeyValue kv = new KeyValue(value, 24);
+        final KeyFrame kf = new KeyFrame(Duration.millis(150), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+
+        contentBody.setCache(true);
+        contentBody.setCacheHint(CacheHint.SPEED);
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(150), contentBody);
+        scaleTransition.setFromX(0.3);
+        scaleTransition.setFromY(0.3);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
         caller.setDisable(true);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(150), contentBody);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        ParallelTransition parallelTransition = new ParallelTransition(scaleTransition, fadeTransition);
+        parallelTransition.play();
+        parallelTransition.setOnFinished((event) -> contentBody.setCacheHint(CacheHint.QUALITY));
+
         this.callerReference = new WeakReference<>(caller);
         this.root.setVisible(true);
     }
@@ -103,10 +143,43 @@ public class FinalDecisionAlertView extends AbstractAlertDialog {
     public void dismiss() {
         Node caller = callerReference.get();
         if (caller != null) {
-            caller.setEffect(null);
-            caller.setDisable(false);
+            GaussianBlur blur = new GaussianBlur(24);
+            caller.setEffect(blur);
+            caller.setCache(true);
+            caller.setCacheHint(CacheHint.SPEED);
+            IntegerProperty value = new SimpleIntegerProperty(0);
+            value.addListener((observable, oldV, newV) -> {
+                if (newV.intValue() == 24) {
+                    caller.setCacheHint(CacheHint.QUALITY);
+                    caller.setEffect(null);
+                    caller.setDisable(false);
+                    return;
+                }
+                blur.setRadius(24 - newV.intValue());
+            });
+            Timeline timeline = new Timeline();
+            final KeyValue kv = new KeyValue(value, 24);
+            final KeyFrame kf = new KeyFrame(Duration.millis(150), kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.play();
+            contentBody.setCache(true);
+            contentBody.setCacheHint(CacheHint.SPEED);
+            ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(150), contentBody);
+            scaleTransition.setFromX(1);
+            scaleTransition.setFromY(1);
+            scaleTransition.setToX(0.3);
+            scaleTransition.setToY(0.3);
+
+            FadeTransition fadeTransition = new FadeTransition(Duration.millis(150), contentBody);
+            fadeTransition.setFromValue(1);
+            fadeTransition.setToValue(0);
+            ParallelTransition parallelTransition = new ParallelTransition(scaleTransition, fadeTransition);
+            parallelTransition.play();
+            parallelTransition.setOnFinished((event) -> {
+                contentBody.setCacheHint(CacheHint.QUALITY);
+                this.root.setVisible(false);
+            });
         }
-        this.root.setVisible(false);
     }
 
     private void setButtonStyle(Button button, ButtonStyle buttonStyle) {
